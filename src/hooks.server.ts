2c096fc5f253e427544e259d63b29e52038257bd
Dhruv <dhruv.k.dev@gmail.com>
2025-12-23 17:38:12 +0530

    Initial Template Setup

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { auth } from '$lib/server/auth';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+
+	if (session) {
+		event.locals.user = session.user;
+		event.locals.session = session.session;
+	} else {
+		event.locals.user = null;
+		event.locals.session = null;
+	}
+
+	return resolve(event);
+};
---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,21 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+import { env } from '$env/dynamic/private';
+import { log } from 'console';
+import { sql } from 'drizzle-orm';
+
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+const client = postgres(env.DATABASE_URL);
+
+// console.log(client);
+
+try {
+    await sql`SELECT 1`;
+    console.log("✅ Database connection successful!");
+} catch (error) {
+    console.error("❌ Database connection failed:", error);
+}
+
+export const db = drizzle(client, { schema });
