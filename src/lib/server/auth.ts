6c19c17533ec2a1b51c46260a9c0ca826c0327ad
Dhruv <dhruvk2028@gmail.com>
2026-02-16 12:02:36 +0530

    SvelteKit Drivers Fix

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -2,18 +2,19 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '$lib/server/db';
 import { user, session, account, verification } from '$lib/server/db/schema';
+import { env } from '$env/dynamic/private';
+
 import {
 	GITHUB_CLIENT_ID,
 	GITHUB_CLIENT_SECRET,
 	GOOGLE_CLIENT_ID,
-	GOOGLE_CLIENT_SECRET,
-	BETTER_AUTH_SECRET
+	GOOGLE_CLIENT_SECRET
 } from '$env/static/private';
 
 export const auth = betterAuth({
-	secret: BETTER_AUTH_SECRET,
-	baseURL: 'http://localhost:5173',
-	trustedOrigins: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+	secret: env.BETTER_AUTH_SECRET!,
+	baseURL: env.BETTER_AUTH_URL!,
+	trustedOrigins: [env.BETTER_AUTH_URL!],
 	database: drizzleAdapter(db, {
 		provider: 'pg', // or "mysql", "sqlite"
 		schema: {
