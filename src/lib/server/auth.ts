8a1a5bd05859a80fe195abe2e02d2b569974bd7d
Dhruv <dhruvk2028@gmail.com>
2026-02-16 12:10:46 +0530

    Better Auth Issue Fix

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -2,40 +2,28 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '$lib/server/db';
 import { user, session, account, verification } from '$lib/server/db/schema';
+import { env } from '$env/dynamic/private';
 
-import { 
-    BETTER_AUTH_SECRET, 
-    BETTER_AUTH_URL,
-    GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET 
-} from '$env/static/private';
+import * as static_env from '$env/static/private';
 
 export const auth = betterAuth({
-	secret: BETTER_AUTH_SECRET,
-    baseURL: BETTER_AUTH_URL,
-    trustedOrigins: [BETTER_AUTH_URL],
+    secret: env.BETTER_AUTH_SECRET || "BUILD_TIME_SECRET_REPLACE_ME_IN_PROD",
+    baseURL: env.BETTER_AUTH_URL || "http://localhost:5173",
+    trustedOrigins: [env.BETTER_AUTH_URL || "http://localhost:5173"],
+    
     database: drizzleAdapter(db, {
         provider: 'pg',
-        schema: {
-            user,
-            session,
-            account,
-            verification
-        }
+        schema: { user, session, account, verification }
     }),
-	emailAndPassword: {
-		enabled: true
-	},
-	socialProviders: {
-		github: {
-			clientId: GITHUB_CLIENT_ID,
-			clientSecret: GITHUB_CLIENT_SECRET
-		},
-		google: {
-			clientId: GOOGLE_CLIENT_ID,
-			clientSecret: GOOGLE_CLIENT_SECRET
-		}
-	}
+    emailAndPassword: { enabled: true },
+    socialProviders: {
+        github: {
+            clientId: static_env.GITHUB_CLIENT_ID || '',
+            clientSecret: static_env.GITHUB_CLIENT_SECRET || ''
+        },
+        google: {
+            clientId: static_env.GOOGLE_CLIENT_ID || '',
+            clientSecret: static_env.GOOGLE_CLIENT_SECRET || ''
+        }
+    }
 });
