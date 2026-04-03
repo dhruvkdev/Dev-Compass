8dfec26ea936eb5e0eaefa28884aeb8f6e9f0e7d
Dhruv <dhruvk2028@gmail.com>
2026-04-03 23:36:16 +0530

    Docker Optimise

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -6,9 +6,9 @@ import * as schema from './schema';
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-if (!DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
-}
+// if (!DATABASE_URL) {
+//   throw new Error('DATABASE_URL is not set');
+// }
 
 const pool = new pg.Pool({
   connectionString: DATABASE_URL,
